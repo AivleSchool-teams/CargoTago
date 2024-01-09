be2c24718c8a34c5a72c5cbf10dbb1b285751a8f
Jaeeun <shape.0514@gmail.com>
2024-01-09 16:21:53 +0900

    차주 화주 chat 연동

--- src/main/front/src/pages/Chat.js
+++ src/main/front/src/pages/Chat.js
@@ -9,7 +9,9 @@ import styles from "./Chat.module.css";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import MicIcon from '@material-ui/icons/Mic';
 import IconButton from '@material-ui/core/IconButton';
-import { socket, username, room, default as ShipperDetail } from './Shipper-Detail';
+import { socket as socket1, room as room1, default as ShipperDetail } from './Shipper-Detail';
+import { socket as socket2, room as room2, default as CarrierDetail } from './Carrier-Detail';
+
 //import { socket, room, default as ShipperDetail } from './Shipper-Detail';
 import axios from "axios";
 
@@ -18,9 +20,12 @@ function Chat() {
     const navigate = useNavigate();
 
     const [username, setUsername] = useState(null);
+    const [socket, setSocket] = useState(io.connect('http://localhost:4000'));
+    const [room, setRoom] = useState(1);
 
 
     useEffect(() => {
+        console.log(username,'zz');
         const token = localStorage.getItem('jwt-token');
         if (!token) {
             // 토큰이 없으면 로그인 페이지로 리디렉션
@@ -37,13 +42,22 @@ function Chat() {
                     console.log('사용자 이름:', response.data.name);
                     setUsername(response.data.name);
 
+
+                    if (response.data.typed === "Shipper") {
+                        setSocket(socket1);
+                        setRoom(room1);
+                    } else {
+                        setSocket(socket2);
+                        setRoom(room2);
+                    }
+
                 })
                 .catch(error => {
                     // 오류 처리
                     console.error('비정상적인 접근입니다.', error);
                 });
         }
-    }, [navigate]);
+    }, []);
 
 
 
@@ -55,6 +69,7 @@ function Chat() {
     const sendMessage = async () => {
         const currentMsg = inputRef.current.value;
         if (currentMsg !== '') {
+            console.log(username,'zz');
             const messageData = {
                 room: room,
                 message: currentMsg,
