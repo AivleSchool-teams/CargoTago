0fff4c7523ad9e2d4026f3426d5f4ddce20587b7
hj <janghyeonji0830@naver.com>
2024-01-10 10:10:18 +0900

    Main페이지 수정.

--- src/main/front/src/pages/Main.js
+++ src/main/front/src/pages/Main.js
@@ -145,7 +145,6 @@ const Main = () => {
 
                                 ))}
                             </Fade>
-
                         </div>
                     </div>
                 </section>
